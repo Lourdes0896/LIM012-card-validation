8a981cc2ec3000acf0c642c73855bc68a8bbc2fd
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-02-12 18:23:55 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import validator from './validator.js';
 
+let Tarjeta
+
 console.log(validator);
+const btn = document.getElementById('btn');
+btn.addEventListener('click', 2do parámetro);
+btn.addEventListener('click', () => {
+    alert(`Hola! Mucho gusto en conocerte`);
+  });
