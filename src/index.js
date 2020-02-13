5dd8837a4f085bfaab69327375fd3c66d1bb6cb5
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-02-13 09:24:28 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import validator from './validator.js';
-
-let Tarjeta
-
-console.log(validator);
 const btn = document.getElementById('btn');
-btn.addEventListener('click', 2do parámetro);
+
 btn.addEventListener('click', () => {
-    alert(`Hola! Mucho gusto en conocerte`);
-  });
+  alert(`Hola! Mucho gusto en conocerte`);
+});
+function formulario (){
+  let nombre,apellidos, email, tarjeta, fecha, vencimiento ;
+  nombres = document.getElementById("nombres").value;
+  apellidos = document.getElementById("apellidos").value;
+  tarjeta = document.getElementById("tarjeta").value;
+  email = document.getElementById("email").value;
+  fecha = document.getElementById("fecha").value;
+  vencimiento = document.getElementById("vencimiento").value;
+}
+
+if ( nombres === "" || apellidos === "" || tarjeta === "" || email === "" || fecha === "" || vencimiento === "" ){
+  alert ("Todos los campos son obligatorios");
+
+}
+
+else if (isNaN (tarjeta)) {
+  alert ("Campo de Numeros")
+
+}
+
+
+
