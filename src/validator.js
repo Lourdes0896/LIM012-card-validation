6fd2b4379cbba960412fdd308581cc8527a08421
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-02-13 10:16:58 -0500

    El boton validar

--- src/validator.js
+++ src/validator.js
@@ -3,3 +3,20 @@ const validator = {
 };
 
 export default validator;
+
+let numeroTarjeta='123456789';
+  let arreglo='';
+  let arreglo2=Array.from(numeroTarjeta);
+  console.log(arreglo2);
+  let invertido=arreglo2.reverse();
+  console.log(invertido);
+  let result=0;
+  for(let i=0;i<invertido.length;i=i+2){
+    //if(invertido[i]%2==0){
+    let duplicado=invertido[i]*2;
+    //let sumar=Array.from(duplicado);
+    console.log(duplicado);
+    
+ }
+
+  
