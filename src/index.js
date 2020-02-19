c23257c885bbd75b1a0bf31cef50d0d37926d9bf
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-02-19 11:24:20 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,75 @@
 import validator from './validator.js';
-const btn = document.getElementById('btn');
 
-btn.addEventListener('click', () => {
-  alert(`Hola! Mucho gusto en conocerte`);
+document.getElementById("seccion2").style.display="none";
+document.getElementById("seccion3").style.display="none";
+document.getElementById("seccion4").style.display="none";
+
+let tarjeta = document.getElementById("creditCardNumber");
+let botonvalidar =document.getElementById("btn");
+let botonregresar =document.getElementById ('verificar');
+let botonmenu = document.getElementById ('menuprincipal');
+
+botonvalidar.addEventListener('click', () => {
+  document.getElementById("seccion1").style.display="none";
+
+  let validadorfinal = document.getElementById('validador');
+  validadorfinal.innerHTML = validator.isValid(tarjeta.value);
+  
+
+  if(validadorfinal.innerHTML === 'true'){
+    let ocultarnumeros = document.getElementById ('maskify');
+    ocultarnumeros.innerHTML = validator.maskify (tarjeta.value);
+    document.getElementById("seccion2").style.display="block";
+  } else{
+    document.getElementById("seccion3").style.display="block";
+  }
+
 });
-function formulario (){
-  let nombre,apellidos, email, tarjeta, fecha, vencimiento ;
-  nombres = document.getElementById("nombres").value;
-  apellidos = document.getElementById("apellidos").value;
-  tarjeta = document.getElementById("tarjeta").value;
-  email = document.getElementById("email").value;
-  fecha = document.getElementById("fecha").value;
-  vencimiento = document.getElementById("vencimiento").value;
-}
 
-if ( nombres === "" || apellidos === "" || tarjeta === "" || email === "" || fecha === "" || vencimiento === "" ){
+botonmenu.addEventListener('click', ()=> {
+  document.getElementById("seccion4").style.display="block";
+  document.getElementById("seccion2").style.display="none";
+
+
+});
+
+botonregresar.addEventListener("click",()=>{
+  document.getElementById("seccion1").style.display="block";
+  document.getElementById("seccion3").style.display="none";
+
+
+ 
+}
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function formulario (){ //tengo que declarar variables fuera
+ 
+/*const nombres = document.getElementById("nombres");
+  const apellidos = document.getElementById("apellidos");
+  
+  const email = document.getElementById("email");
+  const fecha = document.getElementById("fecha");
+  const vencimiento = document.getElementById("vencimiento");
+//}
+*/
+/*if ( nombres === "" || apellidos === "" || tarjeta === "" || email === "" || fecha === "" || vencimiento === "" ){
   alert ("Todos los campos son obligatorios");
 
 }
@@ -22,7 +77,7 @@ if ( nombres === "" || apellidos === "" || tarjeta === "" || email === "" || fec
 else if (isNaN (tarjeta)) {
   alert ("Campo de Numeros")
 
-}
+}*/
 
 
 
